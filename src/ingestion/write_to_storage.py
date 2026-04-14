1872fb38d5b4a71ea3ee60caf6453b81b291b110
Rémi VIALLETON <rvialleton@ippon.fr>
2026-04-14 17:34:45 +0200

    feat(mission4): 1st try

--- src/ingestion/write_to_storage.py
+++ src/ingestion/write_to_storage.py
@@ -1,22 +1,31 @@
+"""Un peu la flemme."""
+
+from .logger.log_handler import log
 from minio import Minio
 from minio.error import S3Error
-from rich import print as rprint
 from datetime import datetime, timedelta
 from typing import Literal
 from io import BytesIO
 
 
 def today_date() -> str:
+    """Un peu la flemme."""
     return datetime.now().strftime("%Y-%m-%d")
 
+
 def day_before_date() -> str:
+    """Un peu la flemme."""
     return (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
 
-def write_to_storage(client: Minio, data: BytesIO, filetype: Literal["parquet", "json", "csv"] = "parquet") -> bool | None:
+
+def write_to_storage(
+    client: Minio, data: BytesIO, filetype: Literal["parquet", "json", "csv"] = "parquet"
+) -> bool | None:
+    """Un peu la flemme."""
     try:
         # Check if the bucket exists
         if not client.bucket_exists(filetype):
-            rprint(f"Bucket '{filetype}' does not exist. Please check your Docker setup.")
+            log.error(f"Bucket '{filetype}' does not exist. Please check your Docker setup.")
             return False
 
         # Create a filename with the current date if it's 9am or later
@@ -28,19 +37,21 @@ def write_to_storage(client: Minio, data: BytesIO, filetype: Literal["parquet", 
         # Check if the object already exists
         objects = client.list_objects(bucket_name=filetype, prefix=filename)
         if any(objects):
-            rprint(f"Object [bold blue]'{filename}'[/bold blue] already exists in bucket [bold yellow]'{filetype}'[/bold yellow].")
+            log.warning(
+                f"Object [bold blue]'{filename}'[/bold blue] already exists in bucket"
+                f"[bold yellow]'{filetype}'[/bold yellow]."
+            )
             return None
 
         # Upload and save the object
         # Here, 'data' is expected to be a bytes-like object
-        result = client.put_object(filetype, filename, data, length=-1, part_size=5*1024*1024)
+        result = client.put_object(filetype, filename, data, length=-1, part_size=5 * 1024 * 1024)
 
-        rprint(
-            "Created [bold blue]{0}[/bold blue] object; etag: [bold red]{1}[/bold red], version-id: [bold magenta]{2}[/bold magenta]".format(
-            result.object_name, result.etag, result.version_id,
-            ),
-               )
+        log.info(
+            f"Created [bold blue]{result.object_name}[/bold blue] object; etag:"
+            f"[bold red]{result.etag}[/bold red], version-id: [bold magenta]{result.version_id}[/bold magenta]"
+        )
         return True
     except S3Error as e:
-        rprint("An error occurred.", e)
+        log.error("An error occurred.", e)
         return False
